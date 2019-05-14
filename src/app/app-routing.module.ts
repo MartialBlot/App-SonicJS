ee35878695a67bf60fd72c12ca22fdd15673be8c
BLOT Martial <martial.blot@outlook.fr>
2019-05-14 15:03:58 +0200

    menu ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { MenuComponent } from './menu/menu.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: '', component: MenuComponent}
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
