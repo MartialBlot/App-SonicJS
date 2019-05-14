f87baf45268fa0ae0beefe7893f04f45ac7d5d78
BLOT Martial <martial.blot@outlook.fr>
2019-05-14 17:37:40 +0200

    stage menu ok

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { MenuComponent } from './menu/menu.component';
+import { LevelsComponent } from './levels/levels.component';
 
 const routes: Routes = [
-  {path: '', component: MenuComponent}
+  {path: '', component: MenuComponent},
+  {path: 'levels', component: LevelsComponent}
+
 
 ];
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MenuComponent } from './menu/menu.component';
+import { LevelsComponent } from './levels/levels.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    MenuComponent
+    MenuComponent,
+    LevelsComponent
   ],
   imports: [
     BrowserModule,
